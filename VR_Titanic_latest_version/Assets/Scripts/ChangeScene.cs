a9d3225baff859dd376a7b18fa18492d56d080fc
manjangoc <manjangoc@gmail.com>
2018-08-04 21:30:51 +0200

    Titanic

---
+++ VR_Titanic_latest_version/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string SzenenName;
+        
+    private void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel(SzenenName);
+    }
+
+}
